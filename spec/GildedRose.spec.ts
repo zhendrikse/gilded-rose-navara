59511017f7e66bafedeee2abcee2e5597d646d18
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-10-21 15:53:06 +0200

    Incorporated approval test framework

--- spec/GildedRose.spec.ts
+++ spec/GildedRose.spec.ts
@@ -1,11 +1,29 @@
 import { Item, GildedRose } from '../src/GildedRose';
+import { verify } from "approvals/lib/Providers/Jest/JestApprovals";
+import { configure } from "approvals/lib/config";
+import { JestReporter } from "approvals/lib/Providers/Jest/JestReporter";
+
+function convert_items_to_string(items = [] as Array<Item>) {
+  let items_as_string = items.map((item) => item.toString() + "\n")
+  return items_as_string.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    "",
+  );
+}
 
 describe('Gilded Rose', () => {
+  beforeAll(() => {
+    configure({
+      reporters: [new JestReporter()],
+    });
+  });
+
   it('updates a foo item', () => {
-    const item =  new Item("Foo", 0, 0);
-    const gildedRose = new GildedRose([item]);
+    const items = [
+      new Item("Foo", 0, 0)
+    ];
+    const gildedRose = new GildedRose(items);
     const updated_items = gildedRose.updateQuality();
-    expect(item.sellIn).toEqual(-1);
-    expect(item.quality).toEqual(0);
+    verify(convert_items_to_string(updated_items));
   });
 });
